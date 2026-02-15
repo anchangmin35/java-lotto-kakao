02c37416f3cce765cb36afda98e4f0e3cc0bba06
paul.an <paul.an@kakaocorp.com>
2026-02-15 22:39:32 +0900

    test: LottoControllerTest 추가
    - 당첨 로또 생성 테스트
    - 당첨 번호, 보너스 번호 중복 시 예외 테스트

--- src/test/java/lotto/controller/LottoControllerTest.java
+++ src/test/java/lotto/controller/LottoControllerTest.java
@@ -9,26 +9,45 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LottoControllerTest {
 
     private final LottoController lottoController = new LottoController();
 
     @Test
+    @DisplayName("당첨 번호와 보너스 번호로 당첨 로또를 생성한다.")
+    public void createWinningLottoTest() {
+        WinningLotto winningLotto = lottoController.createWinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
+
+        assertThat(winningLotto.getWinningLottoNumbers().getLottoNumberList().stream().map(lottoNumber -> lottoNumber.getNumber()).toList())
+                .containsExactly(1, 2, 3, 4, 5, 6);
+        assertThat(winningLotto.getBonusNumber().getNumber()).isEqualTo(7);
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 보너스 번호가 중복되면 당첨 로또 생성에 실패한다.")
+    public void createWinningLottoFailDistinctBonusTest() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> lottoController.createWinningLotto(List.of(1, 2, 3, 4, 5, 6), 6));
+        assertThat(exception.getMessage()).isEqualTo("당첨 번호와 보너스 볼의 번호가 일치합니다.");
+    }
+
+    @Test
     @DisplayName("정답 로또와 구매한 로또 하나를 비교하여 결과 Enum을 반환한다.")
     public void getOneLottoResultTest() {
-        lottoController.createWinningLotto("1, 2, 3, 4, 5, 6", "7");
+        WinningLotto winningLotto = lottoController.createWinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
         Lotto lotto1 = Lotto.from(List.of(1, 2, 3, 4, 5, 6));
         Lotto lotto2 = Lotto.from(List.of(1, 2, 3, 4, 5, 7));
         Lotto lotto3 = Lotto.from(List.of(1, 2, 3, 4, 5, 8));
         Lotto lotto4 = Lotto.from(List.of(1, 2, 3, 4, 9, 8));
         Lotto lotto5 = Lotto.from(List.of(1, 2, 3, 10, 9, 8));
 
-        lotto1.evaluateRank(new WinningLotto("1, 2, 3, 4, 5, 6", "7"));
-        lotto2.evaluateRank(new WinningLotto("1, 2, 3, 4, 5, 6", "7"));
-        lotto3.evaluateRank(new WinningLotto("1, 2, 3, 4, 5, 6", "7"));
-        lotto4.evaluateRank(new WinningLotto("1, 2, 3, 4, 5, 6", "7"));
-        lotto5.evaluateRank(new WinningLotto("1, 2, 3, 4, 5, 6", "7"));
+        lotto1.evaluateRank(winningLotto);
+        lotto2.evaluateRank(winningLotto);
+        lotto3.evaluateRank(winningLotto);
+        lotto4.evaluateRank(winningLotto);
+        lotto5.evaluateRank(winningLotto);
 
         assertThat(lotto1.getLottoRank()).isEqualTo(LottoRank.FIRST);
         assertThat(lotto2.getLottoRank()).isEqualTo(LottoRank.SECOND);
